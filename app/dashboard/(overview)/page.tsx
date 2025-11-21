9fac249bfd7cffe9d644370856cb9d018bedf166
sourov sarker <souravsarker2015@gmail.com>
2025-11-22 02:11:58 +0600

    feat: implement data fetching for dashboard components with suspense

---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,60 @@
+// // /app/dashboard/page.tsx
+//
+// export default function Page() {
+//     return <p>Dashboard Page</p>;
+// }
+
+// /app/dashboard/page.tsx
+
+// import {Card} from '@/app/ui/dashboard/cards';
+// import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+// import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import {lusitana} from '@/app/ui/fonts';
+// import {fetchCardData, fetchLatestInvoices} from "@/app/lib/data";
+import {Suspense} from "react";
+import {CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton} from "@/app/ui/skeletons";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import CardWrapper from "@/app/ui/dashboard/cards";
+
+export default async function Page() { // পেজটি একটি async সার্ভার কম্পোনেন্ট
+
+    // const revenue = await fetchRevenue(); // ডেটা ফেচ করা হলো
+    // const latestInvoices = await fetchLatestInvoices(); // ডেটা ফেচ করা হলো
+    // fetchCardData থেকে মান ডিস্ট্রাকচার করা হলো
+    // const {
+    //     numberOfInvoices,
+    //     numberOfCustomers,
+    //     totalPaidInvoices,
+    //     totalPendingInvoices,
+    // } = await fetchCardData();
+
+    return (
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+                Dashboard
+            </h1>
+            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+                <Suspense fallback={<CardsSkeleton />}> {/* Suspense ব্যবহার করে CardWrapper মোড়ানো */}
+                    <CardWrapper />
+                </Suspense>
+                {/*<Card title="Collected" value={totalPaidInvoices} type="collected"/>*/}
+                {/*<Card title="Pending" value={totalPendingInvoices} type="pending"/>*/}
+                {/*<Card title="Total Invoices" value={numberOfInvoices} type="invoices"/>*/}
+                {/*<Card title="Total Customers" value={numberOfCustomers} type="customers"/>*/}
+            </div>
+            <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+                {/*<RevenueChart revenue={revenue}/>*/}
+                {/*<LatestInvoices latestInvoices={latestInvoices}/>*/}
+
+                <Suspense fallback={<RevenueChartSkeleton />}>
+                    <RevenueChart />
+                </Suspense>
+
+                <Suspense fallback={<LatestInvoicesSkeleton />}> {/* LatestInvoices Suspense বাউন্ডারি */}
+                    <LatestInvoices /> {/* prop মুছে দিন */}
+                </Suspense>
+            </div>
+        </main>
+    );
+}
