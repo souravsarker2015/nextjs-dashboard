06e4f6d353d1acde9c3a05e6d13ac791892c12c3
sourov sarker <souravsarker2015@gmail.com>
2025-11-23 15:23:38 +0600

    feat: add error handling and not found page for invoice management

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -24,6 +24,7 @@ export async function createInvoice(formData: FormData) {
     //     amount: formData.get('amount'),
     //     status: formData.get('status'),
     // };
+
     const {customerId, amount, status} = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
@@ -52,6 +53,7 @@ export async function createInvoice(formData: FormData) {
 const UpdateInvoice = FormSchema.omit({id: true, date: true});
 
 export async function updateInvoice(id: string, formData: FormData) {
+
     const {customerId, amount, status} = UpdateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
@@ -60,13 +62,19 @@ export async function updateInvoice(id: string, formData: FormData) {
 
     const amountInCents = amount * 100;
 
-    await sql`
-        UPDATE invoices
-        SET customer_id = ${customerId},
-            amount      = ${amountInCents},
-            status      = ${status}
-        WHERE id = ${id}
-    `;
+    try {
+        await sql`
+            UPDATE invoices
+            SET customer_id = ${customerId},
+                amount      = ${amountInCents},
+                status      = ${status}
+            WHERE id = ${id}
+        `;
+    } catch (error) {
+        // We'll also log the error to the console for now
+        console.error(error);
+        return {message: 'Database Error: Failed to Update Invoice.'};
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
